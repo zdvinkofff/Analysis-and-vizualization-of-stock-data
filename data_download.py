13839ee737cff092b8296e0ef6137854b7f0ea6a
zdvinkofff <119503489+zdvinkofff@users.noreply.github.com>
2024-04-28 12:13:55 +0300

    Update data_download.py
    Перенос функций из main.py

--- data_download.py
+++ data_download.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+import pandas as pd
 
 
 def fetch_stock_data(ticker, period='1mo'):
@@ -10,3 +11,28 @@ def fetch_stock_data(ticker, period='1mo'):
 def add_moving_average(data, window_size=5):
     data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
     return data
+
+
+def export_data_to_csv(data, filename):
+    try:
+        data.to_csv(filename, index=False)
+        print(f"Данные успешно экспортированы в файл: {filename}")
+    except Exception as e:
+        print(f"Произошла ошибка при экспорте данных в CSV: {e}")
+
+
+def calculate_and_display_average_price(data: pd.DataFrame):
+    average_price = data['Close'].mean()
+    print(f"Средняя цена закрытия акций за заданный период: {average_price}")
+
+
+def notify_if_strong_fluctuations(data: pd.DataFrame, threshold: float):
+    price_max = data['Close'].max()
+    price_min = data['Close'].min()
+    price_range = price_max - price_min
+    fluctuation = (price_range / price_min) * 100
+
+    if fluctuation > threshold:
+        print(f"Цена акций колебалась более чем на {threshold}% за заданный период.")
+    else:
+        print("Цена акций не колебалась на заданный процент за заданный период.")
