93f43a081f096a411f4a2543f75adf1703efe756
Денис <119503489+zdvinkofff@users.noreply.github.com>
2024-05-12 01:51:57 +0300

    Update data_plotting.py

--- data_plotting.py
+++ data_plotting.py
@@ -1,6 +1,57 @@
+import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 import pandas as pd
+import plotly.express as px
 
+# Функция для построения графика линии
+def plot_line_chart(data):
+    plt.figure(figsize=(12, 6))
+    plt.plot(data.index, data['Close'], label='Close Price')
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.title('Stock Closing Price Over Time')
+    plt.legend()
+    plt.show()
+
+# Функция для построения гистограммы
+def plot_histogram(data):
+    plt.figure(figsize=(8, 6))
+    plt.hist(data['Close'], bins=20, color='skyblue', edgecolor='black')
+    plt.xlabel('Price')
+    plt.ylabel('Frequency')
+    plt.title('Distribution of Stock Closing Prices')
+    plt.show()
+
+
+# Функция для построения столбчатой диаграммы
+def plot_bar_chart(data):
+    plt.figure(figsize=(10, 6))
+    plt.bar(data.index, data['Close'], color='skyblue')
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.title('Bar Chart of Stock Closing Prices')
+    plt.show()
+
+
+# Добавим новые типы графиков в список доступных
+available_plots = {
+    'Line Chart': plot_line_chart,
+    'Histogram': plot_histogram,
+    'Bar Chart': plot_bar_chart,
+}
+
+def plot_interactive_average_close(data):
+    average_close = data['Close'].mean()
+
+    fig = go.Figure(data=go.Scatter(x=data.index, y=data['Close'], mode='lines', name='Close Price'))
+    fig.add_hline(y=average_close, line_dash="dot", line_color="red", annotation_text=f'Average Close: {average_close}',
+                  annotation_position="bottom right")
+
+    fig.update_layout(title="Interactive Plot of Close Price with Average Line",
+                      xaxis_title="Date",
+                      yaxis_title="Close Price")
+
+    fig.show()
 def create_and_save_plot(data, ticker, period, filename=None, plot_style='default'):
     plt.figure(figsize=(10, 6))
     plt.style.use(plot_style)
@@ -54,4 +105,5 @@ def plot_macd(data, ticker, period):
     plt.legend()
     plt.savefig(f"{ticker}_{period}_macd_chart.png")
     print("График MACD сохранен")
+
 
